b827eeac6156cbdeb5b9e653e9f28e8ce5cf6198
unknown <mbelhedi@outlook.com>
2021-07-23 19:48:54 +0200

    First Project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Homepage from './homepage.component';
+import './homepage.styles.scss';
+
+function App() {
+  return (
+    <div>
+      <Homepage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/homepage.component.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Homepage=()=>(
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-punkt">
+                <div className="content">
+                    <h1 className="title">Hütte</h1>
+                    <span className="subtitle">Kaufen</span>
+                </div>
+                </div>
+            
+        
+        </div>
+        <div className="menu-punkt">
+        <div className="content">
+            <h1 className="title">Jacken</h1>
+            <span className="subtitle">Kaufen</span>
+        </div>
+        </div>
+        <div className="menu-punkt">
+                <div className="content">
+                    <h1 className="title">Schuhe</h1>
+                    <span className="subtitle">Kaufen</span>
+                </div>
+                </div>
+                </div>
+                
+    
+            
+        
+    
+)
+export default Homepage;
+
