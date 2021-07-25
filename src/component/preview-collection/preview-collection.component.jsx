e4ea1e1d644c334d7f0c33915a24bd70417fc489
unknown <mbelhedi@outlook.com>
2021-07-25 22:54:05 +0200

    Neue Dateien mit Shop Item,new Data Update with new Shop Item files

--- src/component/preview-collection/preview-collection.component.jsx
+++ src/component/preview-collection/preview-collection.component.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './preview-collection.styles.scss'
+import CollectionItem from '../collection-item/collection-item.component';
 
 const PreviewCollection=({title,items})=>(
     <div className="preview-collection">
         <h1 className="title">{title.toUpperCase()}</h1>
         <div className="preview">
             {
-                items.filter((item,idx)=>idx<4).map((item)=>(
-                    <div key={item.id}>{item.name}</div>
+                items.filter((item,idx)=>idx<4).map(({id,...otherItemProps})=>(
+                    <CollectionItem key={id} {...otherItemProps}/>
                 ))
 
             }
