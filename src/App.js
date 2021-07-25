23c3117ad82fe83d0d8f779480b066015c12020e
unknown <mbelhedi@outlook.com>
2021-07-25 20:07:41 +0200

    Das nächste Update,mit neuen Daten, with new Datafiles

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import {Switch,Route } from 'react-router-dom'
 
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
 // import '../pages/homepage/homepage.styles.scss';
-const Capspage=()=>(
-  <div>
-    <h1>Caps Seite</h1>
-  </div>
-)
+
+
 function App() {
   return (
     <Switch>
     <div>
       <Route exact path="/" component={Homepage}/>
-      <Route exact path="/caps" component={Capspage}/>
+      <Route exact path="/shop" component={ShopPage}/>
       
     </div>
     </Switch>
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import SHOP_DATEN from'./shop.data';
+import PreviewCollection from '../../component/preview-collection/preview-collection.component'
+
+class ShopPage extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state={
+            collections: SHOP_DATEN
+        }
+    }
+    render(){
+        const {collections}=this.state;
+        return (<div className="Shop Seite">
+            {
+                collections.map(({id,...otherCollectionProps})=>(
+                    <PreviewCollection key={id}{...otherCollectionProps}/>
+                ))
+
+            }
+
+        </div>)
+    }
+}
+export default ShopPage
