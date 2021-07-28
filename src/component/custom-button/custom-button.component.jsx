aaea694aae15cee46f0ec31de5cc908f13de7604
unknown <mbelhedi@outlook.com>
2021-07-28 08:22:39 +0200

    Der Code mit Login in und Logout eines Google Accounts mit Authentication mit Email und Passwort Hurra es ist vollbracht :), code with Sign in and Out Authentication with Email and Password Hurray its finish :)

--- src/component/custom-button/custom-button.component.jsx
+++ src/component/custom-button/custom-button.component.jsx
@@ -4,7 +4,7 @@ import './custom-button.styles.scss'
 
 const CustomButton=({children,isGoogleSignIn,...otherprops})=>(
     <button className={`${isGoogleSignIn ?'google-sign-in': ''}custom-button`}
-    {...otherprops}>
+    {...otherprops} type="button">
         {children}
 
 
